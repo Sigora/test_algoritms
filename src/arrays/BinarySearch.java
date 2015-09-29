5700504edf735cdb82e008b59712b008b9bd94a4
starnakin <starnakin@provectus-it.com>
2015-09-30 00:10:49 +0300

    added binary search

---
+++ src/arrays/BinarySearch.java
@@ -0,0 +1,66 @@
+package arrays;
+
+/**
+ * Created by starnakin on 29.09.2015.
+ */
+public class BinarySearch {
+    private static int arr[]  = {2, 4, 8, 10, 12, 14};
+    private static int arr2[]  = {1, 6, 11, 16, 21, 31};
+
+    public static void main(String[] args) {
+        System.out.println(searchRecurcive(arr,0,arr.length-1,10));
+
+        System.out.println(searchRecurcive(arr2,0,arr2.length-1,6));
+
+        System.out.println(searchRecurcive(arr,0,arr.length-1,6));
+
+        System.out.println(searchIterrative(arr,10));
+
+       System.out.println(searchIterrative(arr2,6));
+
+        System.out.println(searchIterrative(arr,6));
+    }
+
+    private static int searchRecurcive(int arr[], int start, int end, int number){
+        if(end < start){
+            return -1;
+        }
+
+        int middle = start + (end - start) / 2;
+
+        if(arr[middle] == number){
+            return middle;
+        }
+
+        if(arr[middle] < number){
+            //search in right part
+            return searchRecurcive(arr,middle+1, end, number);
+        }
+        else{
+            //search in left part
+            return searchRecurcive(arr, start, middle-1, number);
+        }
+    }
+
+    public static int searchIterrative(int arr[], int number){
+        int startIndex = 0;
+        int endIndex = arr.length;
+
+        while(startIndex <= endIndex){
+            int middleIndex =startIndex + (endIndex - startIndex) / 2;
+
+            if(arr[middleIndex] == number){
+                return middleIndex;
+            }
+
+            if(arr[middleIndex] < number){
+                startIndex = middleIndex + 1;
+            }
+            else{
+                endIndex = middleIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/arrays/FindMissingNumberOfArithmeticProgression.java
+++ src/arrays/FindMissingNumberOfArithmeticProgression.java
@@ -8,31 +8,39 @@ public class FindMissingNumberOfArithmeticProgression {
     private static int arr2[]  = {1, 6, 11, 16, 21, 31};
 
     public static void main(String[] args) {
-        int diff = getDiff(arr);
-        System.out.println(getMissedElement(arr,0,arr.length-1,diff));
+        System.out.println(getMissedElement(arr,0,arr.length-1,getDiff(arr)));
+
+        System.out.println(getMissedElement(arr2,0,arr2.length-1,getDiff(arr2)));
     }
 
     public static int getDiff(int arr[]){
-        return arr[arr.length-1] - arr[0]/arr.length -1;
+        return (arr[arr.length-1] - arr[0])/arr.length;
     }
 
     public static int getMissedElement(int arr[], int start, int end, int diff){
 
-        if(end <= start){
+        if(end < start){
             return -1;
         }
 
         int middle =start + (end - start)/2;
 
-        if(arr[middle + 1] - arr[middle] !=diff ){
+        if(arr[middle + 1] - arr[middle] != diff ){
             return arr[middle] + diff;
         }
         if(middle > 0 && arr[middle] - arr[middle - 1] != diff){
             return arr[middle] - diff;
         }
 
-
-        if(arr[middle] + diff * )
+        //if difference between middle element - diff * number of elements and start element not equal than
+        //missing element is in left side
+        if(arr[middle] - diff * (middle - start) != arr[start]){
+            return getMissedElement(arr,start,middle - 1,diff);
+        }
+        //search element in right side
+        else{
+            return getMissedElement(arr,middle + 1, end ,diff);
+        }
 
     }
 }
