209f925283700546a766815c6a634a1180fe05e3
starnakin <starnakin@provectus-it.com>
2015-11-09 12:32:57 +0200

    resolved

---
+++ src/algoritms/dinamicprogramming/MinNumberOfCoinsForSum.java
@@ -0,0 +1,13 @@
+package algoritms.dinamicprogramming;
+
+/**
+ * Created by starnakin on 08.11.2015.
+ */
+public class MinNumberOfCoinsForSum {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
+
